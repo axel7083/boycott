bf8e7a77dfda91ca3c3c3269e9f8102bfc5fce48
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-06-24 16:55:00 +0200

    fix: update get_user_usage method

--- packages/backend/api/utils/usage.py
+++ packages/backend/api/utils/usage.py
@@ -1,18 +1,21 @@
 from sqlmodel import Session, select
 
 from core.settings import settings
+from models.tables.asset import Asset
 from models.usage import Usage
 from models.tables.story import Story
 from models.tables.user import User
 
-
 def get_user_usage(user: User, session: Session) -> Usage:
-    statement = select(Story).where(Story.author == user.id)
+    statement = (select(Story, Asset)
+                 .where(Story.author == user.id)
+                 .where(Story.asset_id == Asset.id)
+                 )
 
     results = session.exec(statement)
 
     asset_size_sum = 0
-    for story in results:
-        asset_size_sum = asset_size_sum + story.asset_size
+    for story, asset in results:
+        asset_size_sum = asset_size_sum + asset.asset_size
 
     return Usage(asset_size_sum=asset_size_sum, asset_size_limit=settings.MAX_SUM_STORAGE)
